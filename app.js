cc500a9e0195f4cba382e5d3cc1b98e631729df3
sanjeevyg <yogi.sjv@gmail.com>
2021-05-19 19:28:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+const port = 5000 
+
+app.listen(port, () => {
+    console.log(`listening to port ${port} now`)
+})
+
+
+
+
