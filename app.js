3c48f19293d75e2ac00d656e6213942c768dc41a
sanjeevyg <yogi.sjv@gmail.com>
2021-05-19 21:56:17 -0500

    functions udpated

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const knex = require('knex') //package ./node_modules/knex/index.js
-const config = require('./knexfile')["development"]
-const database = knex(config)
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
+const knex = require('knex')
+const connection = require('./knexfile.js')['development']
+const database = knex(connection)
 
+app.use(cors())
+app.use(express.json())
 
 app.get('/dogs', (request, response) => {
     database('dogs').select()
-            .then(dogs => {
-                response.json({ dogs })     
-            })
+        .then(dogs => {
+            response.json({ dogs })
+        })
 })
 
 app.get('/dogs/:id', (request, response) => {
     database('dogs').select().where({ id: request.params.id }).first()
+        .then( dog => {
+            response.json({ dog })
+        })
+})
+
+app.post('/dogs', (request, response) => {
+    const dog = request.body
+    database('dogs')
+        .insert(dog)
+        .returning('*')
         .then(dog => {
             response.json({ dog })
         })
 })
 
-app.get('/dogs', (request, response) => {
-    dog = body.request 
-    database('dogs')
-        .insert()
-        .returning('*')
-        .then(dog => response.send(dog))
-})
 
-
-
-const port = 5000 
+const port = 5000
 
 app.listen(port, () => {
-    console.log(`listening to port ${port} now`)
+    console.log(`listening to ${port}`)
 })
-
-
-
 
